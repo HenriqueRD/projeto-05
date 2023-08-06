3140933dec4226b0fe5fcd6e00969c9de83dcb11
HenriqueRD <henriquethedordox@gmail.com>
2023-08-06 12:08:42 -0300

    v.1.1

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -24,6 +24,13 @@ const config = {
     name: getAbsolutePath("@storybook/react-vite"),
     options: {},
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/05-design-system/'
+    }
+
+    return config
+  },
   docs: {
     autodocs: "tag",
   },
